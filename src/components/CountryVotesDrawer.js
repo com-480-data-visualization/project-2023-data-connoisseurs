7ca5323d4dc0eb1e43d51fbed317030409cfd34b
jiabaow <jiabaowenwendy@gmail.com>
2023-05-11 22:52:55 +0200

    add bar for country votes out using mock data

--- src/components/CountryVotesDrawer.js
+++ src/components/CountryVotesDrawer.js
@@ -15,6 +15,20 @@ import {
   TabPanels,
   Tabs,
 } from "@chakra-ui/react";
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from "victory";
+
+const data = [
+  {country: 1, points: 1},
+  {country: 2, points: 2},
+  {country: 3, points: 3},
+  {country: 4, points: 4},
+  {country: 5, points: 5},
+  {country: 6, points: 6},
+  {country: 7, points: 7},
+  {country: 8, points: 8},
+  {country: 9, points: 10},
+  {country: 10, points: 12}
+];
 
 export function CountryVotesDrawer({ country, onClose }) {
   const [tabIndex, setTabIndex] = useState(0);
@@ -43,7 +57,23 @@ export function CountryVotesDrawer({ country, onClose }) {
               <Tab key="received-votes">Received Votes</Tab>
             </TabList>
             <TabPanels>
-              <TabPanel>// TODO: votes out by country bar charts</TabPanel>
+              <TabPanel>
+                <VictoryChart
+                  theme={VictoryTheme.material}
+                  domainPadding={{x:5}}
+                >
+                  <VictoryAxis
+                    tickValues={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+                    tickFormat={["UA", "PL", "BE", "CZ", "FR", "FI", "NL", "IT", "SE", "GB"]}
+                  />
+                  <VictoryBar horizontal
+                    data={data}
+                    x="country"
+                    y="points"
+                    labels={({ datum }) => datum.points}
+                  />
+                </VictoryChart>
+              </TabPanel>
               <TabPanel>// TODO: votes in by country bar charts</TabPanel>
             </TabPanels>
           </Tabs>
