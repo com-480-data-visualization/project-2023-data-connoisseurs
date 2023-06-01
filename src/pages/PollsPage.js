a7e1adba282fa6a0ff4a2b24c5635941061d3adc
jiabaow <jiabaowenwendy@gmail.com>
2023-06-01 10:37:50 +0200

    update PollsPage.js

--- src/pages/PollsPage.js
+++ src/pages/PollsPage.js
@@ -1,34 +1,16 @@
 import React from "react";
 import "./PollsPage.css";
-import {
-  VictoryAxis,
-  VictoryBar,
-  VictoryChart,
-  VictoryLabel,
-  VictoryLine,
-  VictoryTheme,
-} from "victory";
-import * as ToggleGroup from "@radix-ui/react-toggle-group";
 import "../components/DropdownMenu.css";
-import { ToggleGroupClass, ToggleItemClass } from "../components/Toggle";
 import { useCallback, useMemo, useState } from "react";
 import countriesByYear from "../data/countries.json";
 import eurovisionworldByYear from "../data/eurovisionworld_by_year.json";
 import ogaeByYear from "../data/ogae_by_year.json";
-import { Dropdown } from "../components/Dropdown";
+import eurovisionworldByCountry from "../data/eurovisionworld_by_country.json";
+import ogaeByCountry from "../data/ogae_by_country.json";
+import {RankingControl} from "../components/RankingControl";
+import {Rankings} from "../components/Rankings";
+import {CountryPollsDrawer} from "../components/CountryPollsDrawer";
 
-const data_per_year = [
-  { country: 1, points: 1 },
-  { country: 2, points: 2 },
-  { country: 3, points: 3 },
-  { country: 4, points: 4 },
-  { country: 5, points: 5 },
-  { country: 6, points: 6 },
-  { country: 7, points: 7 },
-  { country: 8, points: 8 },
-  { country: 9, points: 10 },
-  { country: 10, points: 12 },
-];
 
 const dscYears = Object.keys(countriesByYear).sort().reverse();
 
@@ -43,12 +25,22 @@ export function PollsPage() {
         PollSource.Eurovisionworld
     );
 
+    const [country, setCountry] = useState("Switzerland");
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
     const yearOptions = useMemo(
         () => dscYears.map((year) => ({ label: year, value: year })),
           [dscYears]
     );
+    const pollSourceOptions = useMemo(
+        () => [
+            {value: PollSource.Eurovisionworld, label: "Eurovisionworld"},
+            {value: PollSource.OGAE, label: "OGAE"},
+        ],
+        []
+    );
 
-    const polls = useMemo( () => {
+    const pollsYear = useMemo( () => {
         const format = ([countryCode, points]) => ({
             country: countryCode,
             points: points,
@@ -62,71 +54,53 @@ export function PollsPage() {
         return dataset[year].map(format);
     }, [year, pollSource]);
 
+    const pollsCountry = useMemo(() => {
+        const format = ([year, points]) => ({
+            year: year,
+            points: points,
+        });
+        let dataset = [];
+        if (pollSource === PollSource.Eurovisionworld)
+            dataset = eurovisionworldByCountry;
+        else if (pollSource === PollSource.OGAE)
+            dataset = ogaeByCountry;
+
+        return dataset[country].map(format);
+    }, [country, pollSource]);
+
+
+    const handleToggleDrawer = useCallback(
+        () => setIsDrawerOpen((isOpen) => !isOpen),
+        []
+    );
+
+    const handleClickCountry = useCallback((country) => {
+        setIsDrawerOpen(true);
+        setCountry(country);
+    }, []);
+
   return (
-    <div className="polls-page">
-      <h1>POLLS</h1>
-      <ToggleGroup.Root
-        className={ToggleGroupClass}
-        type="single"
-        value={pollSource}
-        onValueChange={setPollSource}
-      >
-        <ToggleGroup.Item
-          className={ToggleItemClass}
-          value={PollSource.Eurovisionworld}
-        >
-          Eurovisionworld
-        </ToggleGroup.Item>
-        <ToggleGroup.Item className={ToggleItemClass} value={PollSource.OGAE}>
-          OGAE
-        </ToggleGroup.Item>
-      </ToggleGroup.Root>
-
-      <div>
-          <Dropdown value={year} options={yearOptions} onChange={setYear} />
-      </div>
-
-      <div className="two-column-layout">
-        <div className="left-column">
-            <div style={{ overflowX: "auto", maxWidth: "100%" }}>
-                <VictoryChart theme={VictoryTheme.material} height={500}>
-                    <VictoryBar
-                      horizontal
-                      data={polls.slice().reverse()}
-                      x="country"
-                      y="points"
-                      labels={({ datum }) => datum.points}
-                    />
-              </VictoryChart>
-            </div>
-        </div>
-
-        <div className="right-column">
-          <VictoryChart theme={VictoryTheme.material}>
-            <VictoryAxis
-              tickValues={[2016, 2017, 2018, 2019, 2021]}
-              tickFormat={["2016", "2017", "2018", "2019", "2021"]}
+      <main className="relative grow">
+          <RankingControl
+                year={year}
+                yearOptions={yearOptions}
+                onSelectYear={setYear}
+                pollSource={pollSource}
+                pollSourceOptions={pollSourceOptions}
+                onSelectPollSource={setPollSource}
+          />
+          <Rankings
+              data={pollsYear}
+              handleClickCountry={handleClickCountry}
+          />
+          <CountryPollsDrawer
+              data={pollsCountry}
+              isOpen={isDrawerOpen}
+              handleToggle={handleToggleDrawer}
+              country={country}
             />
-            <VictoryAxis dependentAxis tickFormat={(x) => `${x}`} />
-            <VictoryLine
-              style={{
-                data: { stroke: "#c43a31" },
-                parent: { border: "1px solid #ccc" },
-              }}
-              domain={{ x: [2015, 2022], y: [1, 12] }}
-              data={[
-                { x: 2016, y: 4 },
-                { x: 2017, y: 8 },
-                { x: 2018, y: 6 },
-                { x: 2019, y: 3 },
-                { x: 2021, y: 10 },
-              ]}
-              labels={({ datum }) => datum.y}
-              labelComponent={<VictoryLabel renderInPortal dy={-10} />}
-            />
-          </VictoryChart>
-        </div>
-      </div>
-    </div>
+      </main>
   );
 }
+
+
