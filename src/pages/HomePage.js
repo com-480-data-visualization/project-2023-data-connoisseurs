a6518e53d6f6f2b921052b7628b438efda283e68
XioZ <zachary1028@live.com>
2023-06-04 04:23:28 +0200

    Highlights keywords on home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,12 +3,11 @@ import Eurovision from "../assets/eurovision-full.svg";
 
 function Card({ title, text }) {
   return (
-    //dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700
-    <div className="block max-w-sm flex-1 rounded-lg border border-gray-200 bg-primary p-6 shadow hover:bg-secondary ">
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+    <div className="group block max-w-sm flex-1 rounded-lg border border-primary bg-white p-6 shadow hover:border-primary hover:bg-gray-100 ">
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-primary">
         {title}
       </h5>
-      <p className="font-normal text-gray-700 dark:text-gray-300">{text}</p>
+      <p className="font-normal ">{text}</p>
     </div>
   );
 }
@@ -17,24 +16,64 @@ export function HomePage() {
   const contents = [
     {
       title: "Song Contest",
-      text: `The Eurovision Song Contest is an annual music competition that unites
-        countries from Europe and beyond. Each participating country selects a
-        representative or a group to perform an original song on a televised
-        stage. Eurovision allows countries to celebrate their cultural
-        diversity, fosters friendly competition, and culminates in a thrilling
-        voting process where a winner is chosen.`,
+      text: (
+        <>
+          The Eurovision Song Contest is an
+          <span className="group-hover:text-accent">
+            {" annual music competition "}
+          </span>
+          that unites countries from Europe and beyond. Each participating
+          country selects a representative or a group to perform an original
+          song on a televised stage. Eurovision allows countries to celebrate
+          their cultural diversity, fosters friendly competition, and culminates
+          in a thrilling voting process where a winner is chosen.
+        </>
+      ),
     },
     {
       title: "Voting Process",
-      text: `The voting process consists of two components: the jury vote and the televote. The jury vote involves a panel of music industry professionals from each participating country who rank their favourite songs. Their votes contribute to 50% of the overall result. The other 50% comes from the televote, where viewers from each country cast their votes to support their favourite entries.`,
+      text: (
+        <>
+          The voting process consists of two components: the jury vote and the
+          televote. The
+          <span className="group-hover:text-accent"> jury vote </span>involves a
+          panel of music industry professionals from each participating country
+          who rank their favourite songs. Their votes contribute to 50% of the
+          overall result. The other 50% comes from the
+          <span className="group-hover:text-accent"> televote</span>, where
+          viewers from each country cast their votes to support their favourite
+          entries.
+        </>
+      ),
     },
     {
       title: "Voting Blocs",
-      text: `However, over the years, the contest has witnessed the emergence of voting blocs, where neighbouring countries or regions tend to support each other, sometimes leading to predictable voting patterns. While voting blocs are not officially recognised or encouraged, they have become part of the Eurovision lore, adding a unique element of intrigue and sometimes controversy to the competition.`,
+      text: (
+        <>
+          However, over the years, the contest has witnessed the emergence of
+          voting blocs, where
+          <span className="group-hover:text-accent">
+            {" neighbouring countries or regions tend to support each other"}
+          </span>
+          , sometimes leading to predictable voting patterns. While voting blocs
+          are not officially recognised or encouraged, they have become part of
+          the Eurovision lore, adding a unique element of intrigue and sometimes
+          controversy to the competition.
+        </>
+      ),
     },
     {
       title: "Our Visualizations",
-      text: `With our visualisations, voting across the years can be analysed and trends can be uncovered, including whether the voting blocs truly exist. Polls can also be explored to see which songs and countries are fan favourites.`,
+      text: (
+        <>
+          With our visualisations, voting across the years can be analysed and
+          <span className="group-hover:text-accent">
+            {" trends can be uncovered"}
+          </span>
+          , including whether the voting blocs truly exist. Polls can also be
+          explored to see which songs and countries are fan favourites.
+        </>
+      ),
     },
   ];
 
